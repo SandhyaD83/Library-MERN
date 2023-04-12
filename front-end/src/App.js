ef97a5fa9a5d63e72af5d0b96055f239c2c4fac4
“SandhyaD83” <sandhyadandamudi@gmail.com>
2023-04-12 09:06:13 -0400

    created a state variable book and trying to get data into in it using fetch request

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,28 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [book, setBook] = useState([])
+  const getBook = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/books`)
+
+      const data = await response.json();
+
+      console.log(data)
+      setBook(data);
+
+    }
+    catch (e) {
+      console.error(e)
+    }
+  }
+  useEffect(() => {
+    getBook();
+  }, []);
   return (
     <div className="App">
-      App
+      {book}
     </div>
   );
 }
