7be1de20db85452cc7807a6104979d337c02d24e
“SandhyaD83” <sandhyadandamudi@gmail.com>
2023-04-13 13:55:44 -0400

    used find method to make sure the book has the correct author information

--- back-end/controllers/bookcontroller.js
+++ back-end/controllers/bookcontroller.js
@@ -5,7 +5,7 @@ exports.getBooks = async (req, res) => {
         const allBooks = await Book.find({}).populate('author').exec();
         const books = allBooks.map(book => ({
             name: book.name,
-            authorFirstName: book.author.firstName,
+            authorFirstName: book.author,
             image: book.image,
             desc: book.desc,
             price: book.price,
@@ -33,15 +33,18 @@ exports.getAuthors = async (req, res) => {
 exports.createAuthor = async (req, res) => {
     Author.create([{
         firstName: 'Philip',
-        lastName: 'Roth'
+        lastName: 'Roth',
+        books: ['Letting Go', 'The Counterlife']
     },
     {
         firstName: 'Elwyn Brooks',
-        lastName: 'White'
+        lastName: 'White',
+        books: ['Stuart Little', "Charlotte's Web"]
     },
     {
         firstName: 'Peter ',
-        lastName: 'S. Beagle'
+        lastName: 'S. Beagle',
+        books: ['The Last Unicorn', 'The Way Home']
     },
     ], (err, data) => {
         res.redirect('/books/authors');
@@ -49,10 +52,11 @@ exports.createAuthor = async (req, res) => {
 }
 exports.createBooks = async (req, res) => {
     const authors = await Author.find({})
+    authors.find(author => author.books.includes('Letting Go'));
     Book.create([
         {
             name: 'Letting Go',
-            author: authors[0]._id,
+            author: authors.find(author => author.books.includes('Letting Go'))._id,
             image: 'https://upload.wikimedia.org/wikipedia/en/b/b1/Letting_Go_%28novel%29_1st_edition_cover.jpg',
             desc: `The first full-length novel from one of the most renowned writers of the twentieth century, the Pulitzer Prize winning author of American Pastoral, tells the story of a mid-century America and offers “further proof of Mr. Roth's astonishing talent…. Letting Go seethes with life” (The New York Times).`,
             price: '$12.00',
@@ -60,7 +64,7 @@ exports.createBooks = async (req, res) => {
         },
         {
             name: "Charlotte's Web",
-            author: authors[1]._id,
+            author: authors.find(author => author.books.includes("Charlotte's Web"))._id,
             image: 'https://embed.cdn.pais.scholastic.com/v1/channels/tso/products/identifiers/isbn/9780590302715/primary/renditions/700',
             desc: `This is the story of a little girl named Fern who loved a little pig named Wilbur and of Wilbur's dear friend, Charlotte A. Cavatica, a beautiful large grey spider. With the unlikely help of Templeton the rat, and a wonderfully clever plan of her own, Charlotte saves the life of Wilbur, who by this time has grown up to be quite a pig. A time-honoured classic favourite.`,
             price: '$10.00',
@@ -68,7 +72,7 @@ exports.createBooks = async (req, res) => {
         },
         {
             name: 'The Last Unicorn',
-            author: authors[2]._id,
+            author: authors.find(author => author.books.includes('The Last Unicorn'))._id,
             image: 'https://epiloguebookcafe.com/wp-content/uploads/2020/03/last-unicorns.jpg',
             desc: `The unicorn had lived since before memory in a forest where death could touch nothing. Maidens who caught a glimpse of her glory were blessed by enchantment they would never forget. But outside her wondrous realm, dark whispers and rumours carried a message she could not ignore: "Unicorns are gone from the world."`,
             price: '$15.00',
