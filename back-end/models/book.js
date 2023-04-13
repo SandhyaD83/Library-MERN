be65581d5d86350d22cba3f68e0bbb2e5feb4167
“SandhyaD83” <sandhyadandamudi@gmail.com>
2023-04-13 10:58:04 -0400

    changed type of author in bookSchema so it refernces the Author Schema

--- back-end/models/book.js
+++ back-end/models/book.js
@@ -4,13 +4,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     name: { type: String, required: true },
-    author: { type: String, required: true },
-    // authorId: {
-    //     type: ObjectId,
-    //     ref: "Author",
-    //     index: true,
-    //     required: true,
-    // },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Author'
+    },
     image: { type: String, required: true },
     desc: { type: String, required: true },
     price: { type: String, required: true },
