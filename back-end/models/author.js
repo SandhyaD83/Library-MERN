fe7c1a9b46b058302c8b39df7575735f8c5c1d2d
“SandhyaD83” <sandhyadandamudi@gmail.com>
2023-04-13 10:50:42 -0400

    created an author model in backend

--- back-end/models/author.js
+++ back-end/models/author.js
@@ -3,5 +3,5 @@ const Schema = mongoose.Schema;
 const authorSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true }
-})
+}
 module.exports = mongoose.model("Author", authorSchema);
