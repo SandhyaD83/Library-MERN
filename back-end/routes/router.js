4ff889966412f80ecf470944d57cfb3c4a00e9e0
“SandhyaD83” <sandhyadandamudi@gmail.com>
2023-04-13 11:11:29 -0400

    created a router to call crreteAuthor function

--- back-end/routes/router.js
+++ back-end/routes/router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getBooks, createBooks, getNew } = require('../controllers/bookcontroller.js')
+const { getBooks, createBooks, getNew, createAuthor } = require('../controllers/bookcontroller.js')
 router.get('/', getBooks)
 router.get('/seed', createBooks)
+router.get('/author', createAuthor)
 
 
 module.exports = router;
