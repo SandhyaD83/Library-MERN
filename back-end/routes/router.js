b8db7758f6013bb165508f7e52ccab694dafc641
“SandhyaD83” <sandhyadandamudi@gmail.com>
2023-04-17 11:47:22 -0400

    used post method instead of get to create author data

--- back-end/routes/router.js
+++ back-end/routes/router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { getBooks, createBooks, getAuthors, createAuthor, createStatus } = require('../controllers/bookcontroller.js')
 router.get('/', getBooks)
 
-router.get('/author', createAuthor)
+router.post('/author', createAuthor)
 router.get('/authors', getAuthors)
 router.get('/status', createStatus)
 router.post('/post', createBooks)
